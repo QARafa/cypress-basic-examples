d5fd7e7ac33c3d2925c6daaa5908403fabb7fa9d
Rafael Moreira <rafael.santos@foursys.com.br>
2023-01-17 15:37:44 -0300

    ID Project

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require("cypress");
 
-projectId: "9ixjyz"
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    projectId: "9ixjyz"
   },
 });
